c0612a5ee852e0890774f4854dae4391aeb786d0
skatz1990 <skatz1990@gmail.com>
2020-01-30 09:20:19 -0800

    Refactoring complete

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,8 +14,7 @@ module "apache-sg" {
 resource "aws_instance" "web" {
   ami           = lookup(var.ec2_configuration, "ami_id")
   instance_type = lookup(var.ec2_configuration, "instance_type")
-  # vpc_security_group_ids = ["${module.main_sg.this_security_group_id}"]
-  security_groups = ["${module.apache-sg.sg-id}"]
+  vpc_security_group_ids = ["${module.apache-sg.sg-id}"]
   key_name = lookup(var.ec2_configuration, "key_name")
 
   tags = {
@@ -24,9 +23,3 @@ resource "aws_instance" "web" {
 
   user_data = "${file("${lookup(var.ec2_configuration, "user_data_file")}")}"
 }
-
-#Import the elb module
-# module "apache-elb" {
-#   source   = "./modules/elb/"
-#   elb_name = "apache-elb"
-# }
